4d4f69503fa5685fac6b7bd487da5b99964744a3
ankitme2se <ankitme2se@gmail.com>
2025-10-12 17:34:41 +0530

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { error } from 'console';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        rules: {
+            'no-console': 'off',
+            'dot-notation': 'error',
+        },
+    },
+);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 function welcome(name: string) {
     console.log(`Hi ${name}`);
+    const user = {
+        name: 'Ankit',
+    };
+    const u1 = user.name;
+    return name;
 }
 
 welcome('Ankit');
