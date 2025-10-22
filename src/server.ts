9ae1c27f5120b1e3444e0731d66e3a5ed41ff1f0
ankitme2se <ankitme2se@gmail.com>
2025-10-22 15:17:57 +0530

    add express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
 import { Config } from './config/index.js';
+import app from './app.js';
 
-console.log({ port: Config.PORT, env: Config.NODE_ENV });
+const port = Config.PORT;
+const startServer = () => {
+    try {
+        app.listen(Config.PORT, () => {
+            console.log(`app listening on port: ${port}`);
+        });
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
+
+startServer();
